f22f253d904c415234599e0c6520a1792b25d711
Andy <andyivanov2007@gmail.com>
2024-07-16 21:20:29 +0300

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,116 @@
+﻿/*
+Создайте класс очереди с приоритетами для работы с целыми
+значениями. Требуется создать реализации для типичных
+операций над элементами очереди:
+■ IsEmpty - проверка очереди на пустоту;
+■ IsFull - проверка очереди на заполнение;
+■ InsertWithPriority - добавление элемента с приоритетом в очередь;
+PullHighestPriorityElement - удаление элемента с наивысшим приоритетом из очереди;
+■ PullHighestPriorityElement - удаление элемента с
+наивысшим приоритетом из очереди;
+■ Peek - возврат наибольшего по приоритету элемента. Обращаем ваше внимание, что элемент не удаляется из очереди;
+■ Show - вывод всех элементов очереди на экран.
+При показе элемента также необходимо отображать
+приоритет
+*/
+#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+
+void showMenu() {
+    cout << "Меню:\n";
+    cout << "1. Проверка очереди на пустоту\n";
+    cout << "2. Проверка очереди на заполнение\n";
+    cout << "3. Добавление элемента с приоритетом в очередь\n";
+    cout << "4. Удаление элемента с наивысшим приоритетом\n";
+    cout << "5. Показ элемента с наивысшим приоритетом\n";
+    cout << "6. Вывод всех элементов очереди\n";
+    cout << "0. Выход\n";
+    cout << "Выберите пункт меню: ";
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int capacity;
+    cout << "Введите максимальную емкость очереди: ";
+    cin >> capacity;
+
+    Queue<int> queue(capacity);
+
+    int choice;
+    do {
+        showMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            if (queue.IsEmpty()) {
+                cout << "Очередь пуста.\n";
+            }
+            else {
+                cout << "Очередь не пуста.\n";
+            }
+            break;
+
+        case 2:
+            if (queue.IsFull()) {
+                cout << "Очередь заполнена.\n";
+            }
+            else {
+                cout << "Очередь не заполнена.\n";
+            }
+            break;
+
+        case 3:
+            if (!queue.IsFull()) {
+                int num, priority;
+                cout << "Введите число для добавления в очередь: ";
+                cin >> num;
+                cout << "Введите приоритет: ";
+                cin >> priority;
+                queue.InsertWithPriority(num, priority);
+                cout << "Элемент добавлен.\n";
+            }
+            else {
+                cout << "Очередь заполнена, невозможно добавить элемент.\n";
+            }
+            break;
+
+        case 4:
+            if (!queue.IsEmpty()) {
+                int highestPriorityElement = queue.PullHighestPriorityElement();
+                cout << "Удален элемент с наивысшим приоритетом: " << highestPriorityElement << endl;
+            }
+            else {
+                cout << "Очередь пуста, невозможно удалить элемент.\n";
+            }
+            break;
+
+        case 5:
+            if (!queue.IsEmpty()) {
+                int highestPriorityElement = queue.Peek();
+                cout << "Элемент с наивысшим приоритетом: " << highestPriorityElement << endl;
+            }
+            else {
+                cout << "Очередь пуста.\n";
+            }
+            break;
+
+        case 6:
+            cout << "Элементы очереди:\n";
+            queue.show();
+            break;
+
+        case 0:
+            cout << "Выход из программы.\n";
+            break;
+
+        default:
+            cout << "Неверный выбор, попробуйте снова.\n";
+            break;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
---
+++ ConsoleApplication2/Queue.h
@@ -0,0 +1,129 @@
+﻿/*
+Создайте класс очереди с приоритетами для работы с целыми
+значениями. Требуется создать реализации для типичных
+операций над элементами очереди:
+■ IsEmpty - проверка очереди на пустоту;
+■ IsFull - проверка очереди на заполнение;
+■ InsertWithPriority - добавление элемента с приоритетом в очередь;
+■ PullHighestPriorityElement - удаление элемента с наивысшим приоритетом из очереди;
+■ Peek - возврат наибольшего по приоритету элемента. Обращаем ваше внимание, что элемент не удаляется из очереди;
+■ Show - вывод всех элементов очереди на экран.
+При показе элемента также необходимо отображать
+приоритет
+*/
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <class Q>
+class Queue
+{
+	Q* _queue;
+	int* _priority;
+	int _capacity;
+	int _size;
+public:
+	Queue();
+	Queue(int);
+	~Queue();
+	bool IsEmpty();
+	bool IsFull();
+	void InsertWithPriority(Q, int);
+	Q PullHighestPriorityElement();
+	Q Peek();
+	void show();
+};
+
+template<class Q>
+inline Queue<Q>::Queue() : Queue(1) {}
+
+template<class Q>
+inline Queue<Q>::Queue(int capacity) : _capacity(capacity), _size(0)
+{
+	_queue = new Q[_capacity];
+	_priority = new int[_capacity];
+}
+
+template<class Q>
+inline Queue<Q>::~Queue()
+{
+	delete[] _queue;
+	delete[] _priority;
+}
+
+template<class Q>
+inline bool Queue<Q>::IsEmpty()
+{
+	return _size == 0;
+}
+
+template<class Q>
+inline bool Queue<Q>::IsFull()
+{
+	return _size == _capacity;
+}
+
+template<class Q>
+inline void Queue<Q>::InsertWithPriority(Q el, int prior)
+{
+	if (!IsFull())
+	{
+		_queue[_size] = el;
+		_priority[_size] = prior;
+		_size++;
+	}
+}
+
+template<class Q>
+inline Q Queue<Q>::PullHighestPriorityElement()
+{
+	if (!IsEmpty())
+	{
+		int highestPriorityIndex = 0;
+		for (int i = 1; i < _size; i++)
+		{
+			if (_priority[highestPriorityIndex] < _priority[i])
+			{
+				highestPriorityIndex = i;
+			}
+		}
+		Q el = _queue[highestPriorityIndex];
+		for (int j = highestPriorityIndex; j < _size - 1; j++)
+		{
+			_queue[j] = _queue[j + 1];
+			_priority[j] = _priority[j + 1];
+		}
+		_size--;
+		return el;
+	}
+	return Q();
+}
+
+template<class Q>
+inline Q Queue<Q>::Peek()
+{
+	if (!IsEmpty())
+	{
+		int highestPriorityIndex = 0;
+		for (int i = 1; i < _size; i++)
+		{
+			if (_priority[highestPriorityIndex] < _priority[i])
+			{
+				highestPriorityIndex = i;
+			}
+		}
+		return _queue[highestPriorityIndex];
+	}
+	return Q();
+}
+
+template<class Q>
+inline void Queue<Q>::show()
+{
+	cout << "\n-------------------------------------\n";
+	for (int i = 0; i < _size; i++) {
+		cout << _queue[i] << " - " << _priority[i] << "\n";
+	}
+	cout << "-------------------------------------\n";
+}
